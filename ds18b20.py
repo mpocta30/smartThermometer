b455ac8cace687524cba817a0b28329195aacc60
mpocta30 <mpocta30@vt.edu>
2019-12-20 14:00:35 -0500

    Restored ds18b20.py and removed export.txt

--- ds18b20.py
+++ ds18b20.py
@@ -1,15 +1,13 @@
-#from w1thermsensor import W1ThermSensor
+from w1thermsensor import W1ThermSensor
 import random
 
 
 class thermSensor:
     def __init__(self):
-        #self.sensor = W1ThermSensor()
-        self.sensor = "Hello"
+        self.sensor = W1ThermSensor()
 
     def getTemp(self):
-        #cTemp = round(self.sensor.get_temperature(), 2)
-        cTemp = 50
+        cTemp = round(self.sensor.get_temperature(), 2)
         fTemp = round((cTemp*1.8) + 32, 2)
 
         return cTemp, fTemp
